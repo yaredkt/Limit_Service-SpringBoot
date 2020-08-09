7be268c8063de5b89a882ded071d327b82f6ca89
yaried tekie <yaredsonj@gmail.com>
2020-08-09 19:51:05 -0400

    my first commit

---
+++ src/main/java/com/edu/microservice/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,24 @@
+package com.edu.microservice.limitsservice.bean;
+
+public class LimitConfiguration {
+
+	private int maximum;
+	
+	private int minimum;
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+	
+	
+}
---
+++ src/main/java/com/edu/microservice/limitsservice/controller/LimitsConfigController.java
@@ -0,0 +1,34 @@
+package com.edu.microservice.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.microservice.limitsservice.bean.LimitConfiguration;
+import com.edu.microservice.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class LimitsConfigController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping(path = "/limits")
+	public LimitConfiguration retrieveConfiglimits() {
+		
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping(path = "/foult-tolerance/limits")
+	@HystrixCommand(fallbackMethod= "fallbackMethod")
+	public LimitConfiguration retrieveFaoultConfiglimits() {
+		
+		throw new RuntimeException("this is fault toulerance");
+	}
+	
+     public LimitConfiguration fallbackMethod() {
+		
+		return new LimitConfiguration(9999, 9);
+	}
+}
